626ddf585648179257bbcb981a5b2e15e5362088
Himura Kazuto <glagol15@gmail.com>
2016-11-21 13:21:19 +0300

    DB creation finished!

--- Acquire.bat
+++ Acquire.bat
@@ -1,4 +1,5 @@
 @echo off
 
-%APPDATA%\..\Local\Programs\Python\Python35-32\python.exe acquire.py
+rem %APPDATA%\..\Local\Programs\Python\Python35-32\python.exe acquire.py
+"%PROGRAMFILES(X86)%\Python35-32\python.exe" acquire.py
 pause
