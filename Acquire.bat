5b6d3f9647e191b33ad61ad72c5f1280ccd94332
Himura Kazuto <glagol15@gmail.com>
2016-11-25 01:10:44 +0300

    downloaders_beta

--- Acquire.bat
+++ Acquire.bat
@@ -1,5 +1,5 @@
 @echo off
 
-rem %APPDATA%\..\Local\Programs\Python\Python35-32\python.exe acquire.py
-"%PROGRAMFILES(X86)%\Python35-32\python.exe" acquire.py
+%APPDATA%\..\Local\Programs\Python\Python35-32\python.exe acquire.py
+rem "%PROGRAMFILES(X86)%\Python35-32\python.exe" acquire.py
 pause
---
+++ dl_mp3.py
@@ -0,0 +1,59 @@
+import os
+import json
+import sqlite3
+
+db_name = 'sqlite3_data.db'
+
+with open('event_name.txt', 'r') as f:
+    event_name = f.read()
+
+data = dict()
+
+print('Connecting to ' + db_name + '...')
+
+db = sqlite3.connect(os.path.join(event_name, db_name), isolation_level=None)
+c = db.cursor()
+
+c.execute('PRAGMA encoding = "UTF-8"')
+
+c.execute("""
+SELECT  card_code || ' ' || voting_number || '. ' || voting_title AS name,
+        value
+FROM 'values', requests, list
+WHERE   list.id = topic_id AND
+        request_id = requests.id AND
+        type = 'file' AND (
+        'values'.title = 'Минус в формате mp3' OR
+        'values'.title = 'Аудио-трек в формате mp3' OR
+        'values'.title = 'Видеофайл' OR
+        'values'.title = 'Аудио-трек в формате mp3 или видео')
+ORDER BY name
+""")
+
+images = c.fetchall()
+
+name = ""
+counter = 1
+for row in images:
+    prev_name = name
+    name, data = row
+    try:
+        file = json.loads(data)
+        if 'link' in file.keys():
+            file_id = 'link.txt'
+        else:
+            file_id = file['filename']
+
+        if prev_name == name:
+            counter += 1
+            filename = "%s [%d]" % (name, counter)
+        else:
+            filename = name
+            counter = 1
+        filename = filename.encode('cp1251', 'replace').decode('cp1251')
+        filename = ''.join(i if i not in "\/:*?<>|" else "#" for i in filename)
+        print("[OK]", filename + " -> " + file_id)
+    except (TypeError, AttributeError) as e:
+        print("[FAIL]", name)
+
+db.close()
