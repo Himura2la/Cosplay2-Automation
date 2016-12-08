217d7c8f22ec887f0ea5dd605c6b8992ff02d142
Himura Kazuto <glagol15@gmail.com>
2016-12-08 22:26:49 +0300

    renamer mre flexible

--- renamer.py
+++ renamer.py
@@ -4,13 +4,14 @@ import re
 import shutil
 
 db_name = 'sqlite3_data.db'
-input_dir = 'mp3'
+input_dir = 'img'
 with open('event_name.txt', 'r') as f:
     event_name = f.read()
 
+output_dir = os.path.join(event_name, input_dir + '_numbered')
 input_dir = os.path.join(event_name, input_dir)
-output_dir = os.path.join(input_dir, 'out')
-id_regex = re.compile(r"^№ (\d{1,3})\. (.*)(\.\w{2,4})$")
+id_regex = re.compile(r"^№ (?P<num>\d{1,3})\. (?P<name>.*)(?P<ext>\.\w{2,4})$")
+code_regex = re.compile(r"^(?P<code>\w{1,2} \d{3})(?P<ext>\.\w{2,4})$")
 
 print('Connecting to ' + db_name + '...')
 
@@ -27,6 +28,9 @@ WHERE list.id = topic_id AND status = 'approved'
 response = c.fetchall()
 ids = {k: v for k, v in response}
 
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
 for record in os.listdir(input_dir):
     if os.path.isdir(os.path.join(input_dir, record)):
         dir_name = record
@@ -38,14 +42,19 @@ for record in os.listdir(input_dir):
     for f in files:
         if re.search(id_regex, f) is None:
             continue
-        num, name, ext = re.search(id_regex, f).groups()
-        card_code, id = ids[int(num)].split()
+        match = re.search(id_regex, f)
+        num, name, ext = int(match.group('num')), match.group('name'), match.group('ext')
+        try:
+            card_code, id = ids[num].split()
+        except KeyError:
+            print('[WARNING] Extra file:' + f + os.linesep)
+            continue
 
-        new_filename = u"%03d %s. %s%s" % (int(id), card_code, name, ext)
+        new_filename = u"%03d %s. %s(№%d)%s" % (int(id), card_code, name, num, ext)
 
         old_path = os.path.join(input_dir, dir_name, f)
         new_path = os.path.join(output_dir, new_filename)
 
         print(old_path + "\n" + new_path)
-        # shutil.copy(old_path, new_path)
+        shutil.copy(old_path, new_path)
         print("Copied\n")
