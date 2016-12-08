869a05885117e9231d9c27d7049ffa20a0fc4e05
Himura Kazuto <glagol15@gmail.com>
2016-12-08 22:49:45 +0300

    renamed

--- renamer.py
+++ renamer.py
@@ -4,14 +4,21 @@ import re
 import shutil
 
 db_name = 'sqlite3_data.db'
-input_dir = 'img'
+input_dir = 'zad'
 with open('event_name.txt', 'r') as f:
     event_name = f.read()
 
 output_dir = os.path.join(event_name, input_dir + '_numbered')
 input_dir = os.path.join(event_name, input_dir)
-id_regex = re.compile(r"^№ (?P<num>\d{1,3})\. (?P<name>.*)(?P<ext>\.\w{2,4})$")
-code_regex = re.compile(r"^(?P<code>\w{1,2} \d{3})(?P<ext>\.\w{2,4})$")
+id_regex = re.compile(r"^(?P<code>\w{1,2} \d{3})(?P<ext>\.\w{2,4})$")
+
+
+def to_filename(string):
+    filename = string.encode('cp1251', 'replace').decode('cp1251')
+    filename = ''.join(i if i not in "\/*?<>|" else "#" for i in filename)
+    filename = filename.replace(':', " -")
+    filename = filename.replace('"', "'")
+    return filename
 
 print('Connecting to ' + db_name + '...')
 
@@ -20,41 +27,31 @@ c = db.cursor()
 c.execute('PRAGMA encoding = "UTF-8"')
 
 c.execute("""
-SELECT requests.number, card_code||' '||voting_number as id
+SELECT card_code||' '||voting_number, number, voting_title
 FROM list, requests
 WHERE list.id = topic_id AND status = 'approved'
 """)
 
 response = c.fetchall()
-ids = {k: v for k, v in response}
+titles = {k: v for k, _, v in response}
+numbers = {k: v for k, v, _ in response}
 
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-for record in os.listdir(input_dir):
-    if os.path.isdir(os.path.join(input_dir, record)):
-        dir_name = record
-        files = os.listdir(os.path.join(input_dir, record))
-    else:
-        dir_name = ""
-        files = [record]
-
-    for f in files:
-        if re.search(id_regex, f) is None:
+for file in os.listdir(input_dir):
+    path = os.path.join(input_dir, file)
+    if os.path.isfile(path):
+        if re.search(id_regex, file) is None:
             continue
-        match = re.search(id_regex, f)
-        num, name, ext = int(match.group('num')), match.group('name'), match.group('ext')
-        try:
-            card_code, id = ids[num].split()
-        except KeyError:
-            print('[WARNING] Extra file:' + f + os.linesep)
-            continue
+        match = re.search(id_regex, file)
+        code, ext = match.group('code'), match.group('ext')
+        card_code, id = code.split()
+        new_filename = u"%03d %s. %s(№%s)%s" % (int(id), card_code, titles[code], numbers[code], ext)
+        new_filename = to_filename(new_filename)
 
-        new_filename = u"%03d %s. %s(№%d)%s" % (int(id), card_code, name, num, ext)
-
-        old_path = os.path.join(input_dir, dir_name, f)
         new_path = os.path.join(output_dir, new_filename)
 
-        print(old_path + "\n" + new_path)
-        shutil.copy(old_path, new_path)
+        print(path + "\n" + new_path)
+        shutil.copy(path, new_path)
         print("Copied\n")
