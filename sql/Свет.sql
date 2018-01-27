97c8c380e22ad6a8b4fd52089eee2bbd73c080b1
Himura Kazuto <glagol15@gmail.com>
2018-01-27 16:09:21 +0300

    light + art_photo sql

--- sql/Свет.sql
+++ sql/Свет.sql
@@ -1,5 +1,6 @@
 SELECT 
-card_code||' '||voting_number,
+list.title,
+requests.number, --card_code||' '||voting_number,
 voting_title,
 value
 
@@ -9,5 +10,7 @@ list, requests, [values]
 WHERE
 requests.id = request_id AND
 list.id = topic_id AND
-status = 'approved' AND
+--status = 'approved' AND
 [values].title = 'Пожелания по сценическому свету (необязательно)'
+
+order by list.title
