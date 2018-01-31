af73d36437266a81a46213a45984fe3ffaff0ddd
himura <himura@kampai-nb>
2018-01-31 13:45:38 +0300

    program check

--- testcafe/testcafe-broadcaster.js
+++ testcafe/testcafe-broadcaster.js
@@ -2,13 +2,12 @@ import { Selector } from 'testcafe';
 
 
 const event_name = 'tulafest'
-const token = '___Bake_a_Cookie___'
+const token = ''
 
-const message1 = "Напоминаем, что Ваша заявка на Тульский фест YnO еще не до конца оформлена, а досыл закрывается в это воскресенье. Подробности на к2."
-const message2 = "Добрый вечер! Пожалуйста, заполните все необходимые поля заявки на Тульский фест Yuki no Odori до 21.01.18, иначе она будет автоматически отклонена и организаторам будет Очень грустно Т_Т. Со своей стороны, делимся с вами (пока) тайной информацией, что датой фестиваля назначено 17 февраля. Сообщить это публично мы сможем уже очень скоро, но Вы узнаёте дату в числе первых. Очень ждём Вашего досыла ^_^"
+const message1 = "Приглашаем Вас проверить расположение своего номера в черновом вариане программы фестиваля: https://docs.google.com/spreadsheets/d/1HkcKL72kTZPipqMU9M-CbKMP2b8p7uH46rtIRIhh42w/edit?usp=sharing Разумеется, время указано приблизительное и на него ни в коем случае на стоит ориентироваться. Номера также будут меняться, чтобы соответствовать порядку выступлений. Если Вас что-то не устраивает в программе, напишите пожалуйста комментарий к Вашей ячейке в таблице. Также, для надёжности, можно написать сюда, Дере VK или мне VK."
 
 
-var start_i = 11  // If it fails in da middle, you can continue
+var start_i = 0  // If it fails in da middle, you can continue
 var start_j = 0
 
 fixture `Broadcaster`.page `http://${event_name}.cosplay2.ru/orgs`;
@@ -33,38 +32,37 @@ test('Write to All', async t => {
         var nums = side_topics.find('a').withText('№')
         var nums_count = await nums.count
         for (var j = start_j; j < nums_count; j++){
-            if (j > 3) break
             console.log(`(${i}, ${j}) `, await nums.nth(j).innerText)
             await t.click(nums.nth(j))
             // In a request
             var status = await Selector('a[editable-select="$ctrl.request.status"]').innerText
             console.log(status)
-            if (status.indexOf("НУЖЕН ВАШ ОТКЛИК") > 0 || status.indexOf("НУЖНЫ ДОПОЛНИТЕЛЬНЫЕ МАТЕРИАЛЫ") > 0){
+            if (status.indexOf("ЗАЯВКА ПРИНЯТА") > 0){
                 await t
                     .click(Selector('a[ng-click="$ctrl.newCommentFormVisible = true"]'))
                     .typeText(Selector('textarea[ng-model="$ctrl.newCommentForm.comtext"]'), message1)
-                    //.click(Selector('input[ng-model="$ctrl.newCommentForm.email"]'))
-                    .click(Selector('input[ng-model="$ctrl.newCommentForm.sms"]'))
+                    .click(Selector('input[ng-model="$ctrl.newCommentForm.email"]'))
+                    //.click(Selector('input[ng-model="$ctrl.newCommentForm.sms"]'))
                     .click(Selector('button[type=submit]'))  // Жмем кнопку "Отправить"
                 
                 var last_comment = Selector('tr[ng-repeat="comment in $ctrl.comments track by comment.id"]')
                                     .find('span[ng-bind-html="comment.content | htmltext"]').nth(-1)
                 await t.expect(last_comment.innerText).eql(message1)  // Проверяем, что отправилось
             
-            await t
-                .click(Selector('a[ng-click="$ctrl.newCommentFormVisible = true"]'))
-                    .typeText(Selector('textarea[ng-model="$ctrl.newCommentForm.comtext"]'), message2)
-                .click(Selector('input[ng-model="$ctrl.newCommentForm.email"]'))
-                    .click(Selector('input[ng-model="$ctrl.newCommentForm.sms"]')) // СНИМАЕМ чекбокс
-                    .click(Selector('button[type=submit]'))  // Жмем кнопку "Отправить"
-                
-                last_comment = Selector('tr[ng-repeat="comment in $ctrl.comments track by comment.id"]')
-                                .find('span[ng-bind-html="comment.content | htmltext"]').nth(-1)
-                await t.expect(last_comment.innerText).eql(message2)  // Проверяем, что отправилось
+          //  await t
+          //      .click(Selector('a[ng-click="$ctrl.newCommentFormVisible = true"]'))
+          //          .typeText(Selector('textarea[ng-model="$ctrl.newCommentForm.comtext"]'), message2)
+          //      .click(Selector('input[ng-model="$ctrl.newCommentForm.email"]'))
+          //          .click(Selector('input[ng-model="$ctrl.newCommentForm.sms"]')) // СНИМАЕМ чекбокс
+          //          .click(Selector('button[type=submit]'))  // Жмем кнопку "Отправить"
+          //      
+          //      last_comment = Selector('tr[ng-repeat="comment in $ctrl.comments track by comment.id"]')
+          //                      .find('span[ng-bind-html="comment.content | htmltext"]').nth(-1)
+          //      await t.expect(last_comment.innerText).eql(message2)  // Проверяем, что отправилось
                 
             
                 console.log("Sent!")
-            //await t.debug()
+            await t.debug()
             }
         }
         start_j = 0
