d87b83de4bd62b1246a8c02043114aa68d362777
Alexander Yakovlev <keloero@oreolek.ru>
2018-06-11 17:21:33 +0700

    config

--- mktex.py
+++ mktex.py
@@ -28,8 +28,16 @@ texcode = ''
 def opt(a, k, pre=''):
     return pre + a[k] if k in a else ''
 
+def getfield(fields, config, options):
+    for field in config[options]:
+        try:
+            return fields[field]
+        except KeyError:
+            continue
+    return ''
 
 for target_dir in target_dirs:
+    nom = target_dir
     base_dir = os.path.join(fest_path, target_dir)
     for art_dir in os.listdir(os.path.join(fest_path, target_dir)):
         num = re.findall(r'\d+', art_dir.split('. ')[0])[0]
@@ -52,7 +60,7 @@ for target_dir in target_dirs:
                    REPLACE(GROUP_CONCAT(DISTINCT value), ',', ', ') as value,
                    status,
                    requests.id as url_id,
-				   voting_number
+                   voting_number
             FROM requests, [values]
             WHERE requests.id = request_id
             AND requests.number = ?
@@ -86,48 +94,31 @@ for target_dir in target_dirs:
             if voting_number != None:
                 num = 'ART~%d' % voting_number
             else:
-                num = 'ART~%d' % num
-            authors_cat = 'Авторы'
+                if config['dry_run']:
+                    num = 'ART~%d' % num
         else:
             if voting_number != None:
                 num = 'FC~%d' % voting_number
             else:
-                num = 'FC~%d' % num
-            authors_cat = 'Косплееры'
+                if config['dry_run']:
+                    num = 'FC~%d' % num
 
         try:
-            nom = fields['Информация о работе.Номинация']
-            contest = fields['Информация о работе.Участие в конкурсе']
-            nicks = fields[authors_cat + '.Ник']
-            nicks = "%s: %s" % (authors_cat if ',' in nicks else authors_cat[:-1], nicks)
-            city = fields[authors_cat + '.Город']
-            title = opt(fields, 'Информация о работе.Название работы')
-            fandom = opt(fields, 'Информация о работе.Фэндом(ы)')
-            photographer_nick = opt(fields, 'Фотографы (необязательно).Ник')
-            photographer_name = opt(fields, 'Фотографы (необязательно).Имя') + opt(fields, 'Фотографы (необязательно).Фамилия', ' ')
-            photographer_team = opt(fields, 'Фотографы (необязательно).Команда/сообщество фотографов (необязательно)')
+            nick = getfield(fields, config, 'name_fields')
+            city = fields[config['city_field']]
+            title = getfield(fields, config, 'name_fields')
+            fandom = getfield(fields, config, 'fandom_fields')
+            extra = getfield(fields, config, 'extra_fields')
+            other_authors = getfield(fields, config, 'other_authors_fields')
         except KeyError as e:
             texcode += "%% [!!!! ERROR !!!!] No value for '%s' in '%s' (status: %s)\n" % (e.args[0], art_dir, status)
             continue
-        
-        if fandom not in title:
-            title = "%s (%s)" % (title, fandom) if title and fandom else title if title \
-                                                else fandom if fandom else "Без названия"
 
-        nom = nom if contest == 'В конкурсе' else contest
-        extra = ''
-        if photographer_nick or photographer_name or photographer_team:
-            if photographer_nick or photographer_name:
-                photographer = photographer_nick if photographer_nick else photographer_name
-                if photographer_team:
-                    photographer = "%s (%s)" % (photographer, photographer_team)
-            else:
-                photographer = photographer_team
-            photographer = "%s: %s" % ('Фотографы' if ',' in photographer else 'Фотограф', photographer)
-            extra += photographer
+        if other_authors != None:
+            extra += other_authors
 
         texcode += '\\imgportrait' if portrait else '\\imglandscape'
-        texcode += '{%s}{%s, г.%s}{%s}{%s}{%s}{%s}{%s}\n' % (num, nicks, city, title, nom, extra, path, url_id)
+        texcode += '{%s}{%s, г.%s}{%s}{%s}{%s}{%s}{%s}\n' % (num, nick, city, title, nom, extra, path, url_id)
 
 texcode.replace('&', '\&')
 
