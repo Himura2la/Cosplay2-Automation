2d5018511d224ab44da3657367de748bb935744f
Himura Kazuto <glagol15@gmail.com>
2018-09-30 17:53:21 +0300

    Fix gallery-generator

--- etc/gallery-gen.py
+++ etc/gallery-gen.py
@@ -1,10 +1,4 @@
-
-
 import vk
-
-# https://oauth.vk.com/authorize?v=5.85&response_type=token&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,groups,photos&client_id=[INSERT_ID_HERE]
-session = vk.Session(access_token='')
-VK = vk.API(session)
 
 request = """
 https://vk.com/photo-91388480_456240395
@@ -37,11 +31,14 @@ portrait_thumb_size = 'o'
 landscape_thumb_size = 'p'
 full_size = 'w'
 
+# https://oauth.vk.com/authorize?v=5.85&response_type=token&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,groups,photos&client_id=[INSERT_ID_HERE]
+session = vk.Session(access_token='')
+VK = vk.API(session)
 photos = ",".join(request.replace('https://vk.com/photo', '').split())
 response = VK.photos.getById(v='5.85', photos=photos)
 
 portrait_count, landscape_count = 0, 0
-result_html = '<p class="gallery"><div class="gallery-block">\n'
+result_html = '<div class="gallery"><div class="gallery-block">\n'
 
 for i, photo in enumerate(response):
     full = filter(lambda size: size['type'] == full_size, photo['sizes']).__next__()
@@ -52,8 +49,8 @@ for i, photo in enumerate(response):
         thumb_size = portrait_thumb_size
         portrait_count += 1
     thumb = filter(lambda size: size['type'] == thumb_size, photo['sizes']).__next__()
-    html = f'<a class="gallery-image" href="{full["url"]}" data-lightbox="roadtrip">' \
-           f'<img class="gallery-image" src="{thumb["url"]}"></a>'
+    html = f'<a href="{full["url"]}" data-lightbox="roadtrip">' \
+           f'<img class="gallery-image-{thumb_size}" src="{thumb["url"]}"></a>'
     result_html += html
     if i != len(response) - 1:
         if landscape_count >= 2:
@@ -62,6 +59,36 @@ for i, photo in enumerate(response):
         if portrait_count >= 3:
             portrait_count = 0
             result_html += '\n</div><div class="gallery-block">\n'
-result_html += '\n</div></p>'
+result_html += '\n</div></div>'
 
 print(result_html)
+
+
+# .gallery {
+#     margin-left: -5px;
+#     margin-right: -5px;
+# }
+# div.gallery-block {
+#     display: inline-block;
+# }
+# img.gallery-image-o, img.gallery-image-p {
+#     margin: 5px;
+#     border-radius: 5px;
+# }
+# iframe.landing-video {
+#     width: 830px;
+#     height: 457px;
+# }
+#
+# @media (max-width: 467px) {
+#     img.gallery-image-o {
+#         width: 90px;
+#         height: auto;
+#         margin: 2px;
+#     }
+#     img.gallery-image-p {
+#         width: 137px;
+#         height: auto;
+#         margin: 2px;
+#     }
+# }
