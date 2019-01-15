b91813d40f3ac9bf808c9598b8d77225f565df6a
Himura Kazuto <glagol15@gmail.com>
2019-01-15 22:19:23 +0300

    tweak sql

--- sql/program_wishes.sql
+++ sql/program_wishes.sql
@@ -1,7 +1,6 @@
 SELECT	"http://tulafest.cosplay2.ru/orgs/requests/request/" || requests.id as link,
-		requests.number,
-		voting_title, contest,
-		time, cities, wish
+		requests.number, list.title, contest,
+		voting_title, duration, cities, wish
 
 FROM list, requests
 
@@ -10,7 +9,7 @@ LEFT JOIN ( SELECT request_id as con_rid, value as contest
 			WHERE title == 'Участие в конкурсе')
 	ON con_rid = requests.id
 
-LEFT JOIN ( SELECT request_id as t_rid, ROUND(value, 2) || " (" || REPLACE(ROUND(value) - 1, '.0', '') || "m " || REPLACE(round(value*60)%60, '.0', '') || "s)" as time
+LEFT JOIN ( SELECT request_id as t_rid, CAST(value AS INT) || "m " || CAST(ROUND(value*60)%60 AS INT) || "s" as duration
 			FROM [values] 
 			WHERE title LIKE 'Продолжительность%')
 	ON t_rid = requests.id
@@ -26,6 +25,6 @@ LEFT JOIN ( SELECT request_id as w_rid, value as wish
 			WHERE title == 'Пожелание по расположению номера в программе (необязательно)')
 	ON w_rid = requests.id
 	
-WHERE	list.id = topic_id AND
-		status != 'disapproved'
-		and list.card_code not in ('ART', 'FC', 'ACF')
+WHERE	list.id = topic_id
+		AND status != 'disapproved'
+		AND default_duration > 0
