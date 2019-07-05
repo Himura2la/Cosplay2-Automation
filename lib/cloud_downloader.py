f7287db835beffc2405a8d1a010dd80fe38bab00
Himura Kazuto <glagol15@gmail.com>
2019-07-05 22:26:36 +0300

    Yandex and mailru done

--- lib/cloud_downloader.py
+++ lib/cloud_downloader.py
@@ -3,6 +3,8 @@
 # Author: Himura Kazuto <himura@tulafest.ru>
 
 import youtube_dl
+import requests
+import json
 
 
 class CloudDownloader(object):
@@ -13,20 +15,34 @@ class CloudDownloader(object):
             'outtmpl': f'{target}.%(ext)s'
         }
         print(f'Downloading {url} to {target}...')
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            try:
-                ydl.download([url])
-                return True
-            except Exception as e:
-                print(e)
-                return False
+        if '://yadi.sk/i' in url:
+            print('Getting direct link from YaDisk API...')
+            return CloudDownloader.get_link_yadisk(url)
+        elif '://cloud.mail.ru/public/' in url:
+            print('Getting direct link from Mail.Ru Cloud download page...')
+            return CloudDownloader.get_link_mailru(url)
+        else:
+            print('Using YoutubeDL...')
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                try:
+                    ydl.download([url])
+                    return True
+                except Exception as e:
+                    print(e)
+                    return False
 
+    @staticmethod
+    def get_link_yadisk(url):
+        YADISK_ENDPOINT = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?public_key={}'
+        return requests.get(YADISK_ENDPOINT.format(url)).json()['href']
 
-if __name__ == "__main__":
-    import json
-
-    links = []
-
-    for i, link in enumerate(links):
-        url = json.loads(link)['link']
-        CloudDownloader.get(url, f'.\\{i}')
+    @staticmethod
+    def get_link_mailru(url):
+        CLOUD_MAILRU_TOKEN_URL = 'https://cloud.mail.ru/api/v2/tokens/download'
+        page = requests.get(url).text
+        part1 = url.split('/public/', 1)[-1]
+        weblink_section = page.rsplit('"weblink_get": [', 1)[-1].split(']', 1)[0]
+        part0 = json.loads(weblink_section)['url']
+        token = requests.get(CLOUD_MAILRU_TOKEN_URL).json()['body']['token']
+        filename = ''
+        return f'{part0}/{part1}/{filename}?key={token}'
