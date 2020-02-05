30a0ea3c0f6fa00b50183a06063408d028624f37
Himura Kazuto <glagol15@gmail.com>
2020-02-06 01:18:12 +0300

    days to fest

--- analyze.py
+++ analyze.py
@@ -7,40 +7,45 @@ import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.dates import (DAILY, DateFormatter, rrulewrapper, RRuleLocator, drange)
 
-db_dirs = {
-    'yno8': r"C:\Users\himura\Desktop\yno8-backups",
-    'yno9': r"C:\Users\himura\Desktop\yno9-backups"
-}
+db_dirs = [
+    r"C:\Users\himura\Desktop\yno8-backups",
+    r"C:\Users\himura\Desktop\yno9-backups"
+]
 queries = {
     'scene': 'select count(*) from list, requests where list.id = topic_id and default_duration > 0 and card_code not like "V%"'
 #    'etickets': 'select count(*) from etickets where money_transfered not null'
 }
 line_colors = ['-b', '-r']
 
-# TODO: count days to fest
-
 data = {}
-for fest, d in db_dirs.items():
+for d in db_dirs:
+    fest_info = None
     for f in glob(os.path.join(d, '*.db')):
-        dir_path = os.path.join(d, f)
         results = {}
-        date = datetime.strptime(f.rsplit('\\', 1)[1], '%y-%m-%d_%H-%M-%S.db')
+        date = datetime.strptime(os.path.split(f)[1], '%y-%m-%d_%H-%M-%S.db')
         with sqlite3.connect(f, isolation_level=None) as db:
             c = db.cursor()
             c.execute('PRAGMA encoding = "UTF-8"')
-            for name, query in queries.items():
+            if not fest_info:
+                c.execute('select * from settings where key in ("short_title", "start_time")')
+                fest_info = { k: v for k, v in c.fetchall() }
+                fest_info['start_time'] = datetime.strptime(fest_info['start_time'], '%d.%m.%y %H:%M')
+            for query_name, query in queries.items():
                 c.execute(query)
                 result, = c.fetchone()
-                results[f'{fest}-{name}'] = result
-            data[date] = (date, results)
+                results[f'{fest_info["short_title"]}-{query_name}'] = result
+        days_to_fest = int((fest_info['start_time'] - date).days)
+        if not days_to_fest in data:
+            data[days_to_fest] = results
+        else:
+            for dataset in results:
+                data[days_to_fest][dataset] = results[dataset]
 
 fig, ax = plt.subplots()
 x = list(data.keys())
-for i, name in enumerate(data[x[0]][1].keys()):
-    ax.plot_date(x, [v[name] for k, v in data.values()], line_colors[i])
-ax.xaxis.set_major_formatter(DateFormatter(f'%d.%m'))
-ax.xaxis.set_major_locator(RRuleLocator(rrulewrapper(DAILY, interval=7)))
-ax.xaxis.set_tick_params(rotation=30)
+datasets = data[x[0]].keys()
+for i, name in enumerate(datasets):
+    ax.plot(x, [v[name] if name in v else None for k, v in data.items()], line_colors[i])
 ax.grid()
-ax.legend(queries.keys())
+ax.legend(datasets)
 plt.show()
