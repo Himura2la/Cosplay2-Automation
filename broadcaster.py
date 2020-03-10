190b6c565658e7d3d6f55a6367595ed61040bd20
Himura Kazuto <glagol15@gmail.com>
2020-03-11 00:49:09 +0300

    Promocodes broadcast

--- broadcaster.py
+++ broadcaster.py
@@ -2,12 +2,27 @@
 # -*- coding: utf-8 -*-
 
 comment = """
-Вашей заявке присвоен номер {} по программе (выступление в блоке {})! Конечный вариант программы фестиваля уже ВК!
-""".strip()  # --------------------------------------------- 1 SMS ->|      --------- 2 SMS with "YnO9, заявка 000: " ->|
-target = "status = 'approved' and default_duration > 0"
+Косплеер, лови свой промокод на билеты со скидкой! {}
+""".strip()  # --------------------------------------------- 1 SMS ->|
+target = "[values].title == 'Количество участников' AND status = 'disapproved' AND card_code = 'D'"
 email = True
 sms = True
 
+def prepare_comment(details, field_value):
+    n_persons = int(field_value)
+    promocodes = variable_values.pop()
+    var_comment = "Косплееры, ловите свои промокоды на билеты со скидкой! {}" if n_persons > 1 else comment
+    for i in range(n_persons - 1):
+        promocodes += ', ' + variable_values.pop()
+    return var_comment.format(promocodes)
+
+    # voting_number = details.split('] ', 1)[1].split('. ', 1)[0]
+    # comment.format(voting_number, voting_number.split(' ', 1)[1][0])
+
+variable_values = """
+test1
+test2
+""".strip().split('\n')
 
 from lib.authenticator import Authenticator
 from lib.api import Cosplay2API, Requester
@@ -29,15 +44,20 @@ with sqlite3.connect(db_path, isolation_level=None) as db:
         SELECT DISTINCT
             requests.id,
 	        '[' || status || '] ' || IFNULL(list.card_code || ' ' || voting_number, '№ ' || requests.number) || IFNULL('. ' || voting_title, user_title) AS details,
+            [values].value
         FROM requests, list, [values]
         WHERE topic_id = list.id
           AND request_id = requests.id
           AND {target}""")
-    target_requests = [(r_id, details) for r_id, details in c.fetchall()]
+    target_requests = [(r_id, details, field_value) for r_id, details, field_value in c.fetchall()]
 
 print(f'Requests to add the comment to:')
-[print(f'{i+1}. {details} ({api.request_url(r_id)})') for i, (r_id, details) in enumerate(target_requests)]
-print(f"\nThe comment's text ({len(comment)} characters, {1+len(comment)//70} SMS):\n{comment}")
+[print(f'{i+1}. {details} [{field_value}] ({api.request_url(r_id)})') for i, (r_id, details, field_value) in enumerate(target_requests)]
+print(f"\nVariable values: {len(variable_values)}")
+test_comment = prepare_comment(target_requests[0][1], target_requests[0][2])
+print(f"The comment's text ({len(test_comment)} characters, {1+len(test_comment)//70} SMS):\n{test_comment}")
+if len(target_requests) > len(variable_values):
+    print(f'WARNING!!! Variable values are not enough for all requests!!!')
 if not input('\nDo it (yes/no)?: ').lower() in ('y', 'ye', 'yes', 'yep', 'д', 'да'):
     print('You are not ready yet! Please, double check everything and type "yes" to continue.')
     exit()
@@ -50,12 +70,12 @@ r = Requester(a.cookie)
 
 done_requests = set()
 try:
-    for request_id, details in target_requests:
+    for request_id, details, field_value in target_requests:
         voting_number = details.split('] ', 1)[1].split('. ', 1)[0]
-        ready_comment = comment.format(voting_number, voting_number.split(' ', 1)[1][0])
+        ready_comment = prepare_comment(details, field_value)
         if not r.request(api.add_comment_POST, {"request_id": request_id, "comment": ready_comment, "email": email, "sms": sms}):
             raise Exception("Maybe you ran out of SMS money...")
-        print(f'✔️ {details} ({api.request_url(request_id)})')
+        print(f'✔️ {details} [{field_value}] ({api.request_url(request_id)})')
         print(ready_comment)
         done_requests.add(str(request_id))
         sleep(1)
