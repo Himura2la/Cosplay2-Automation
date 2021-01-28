1c8c30f9257af23c5d30b8bce4fa3b21e2cb7780
Himura Kazuto <glagol15@gmail.com>
2021-01-28 16:12:06 +0300

    Small fixes

--- sql/check_announcements.sql
+++ sql/check_announcements.sql
@@ -6,6 +6,6 @@ FROM list, requests
 WHERE
 	list.id = topic_id
 	AND	status in ('approved')
-	AND default_duration > 0
+	AND (default_duration > 0 OR card_code in ('V', 'VC'))
 
 ORDER BY voting_number
