04726f8e712c7279b7b61bdee03a9d52f15e9b5b
Himura Kazuto <glagol15@gmail.com>
2021-07-14 00:29:28 +0300

    improve participants table

--- sql/participants_list.sql
+++ sql/participants_list.sql
@@ -1,11 +1,14 @@
 
 SELECT DISTINCT
     last_name, first_name, mid_name, city,
-    group_concat(distinct nick) as nick,
-    group_concat(distinct phone) as phone,
+    group_concat(distinct nick) as 'Ник',
+	group_concat(distinct pron) as 'Транскрипция',
+    ''''||group_concat(distinct phone) as 'Телефон',
 	replace(group_concat(distinct card_code ||' '|| voting_number),',',', ') as 'Номера',
 	replace(group_concat(distinct '№ '|| number),',',', ') as 'Заявки',
-	group_concat(distinct list.title) as 'Разделы', group_concat(distinct section_title) as 'Секции'
+	group_concat(distinct list.title) as 'Разделы', group_concat(distinct section_title) as 'Секции',
+    group_concat(distinct fandom) as 'Фэндомы',
+    ifnull(group_concat(distinct chars), single_char) as 'Персонажи'
 FROM list, requests, [values]
 
 LEFT JOIN ( SELECT request_section_id as ln_rsid, value as last_name
@@ -24,6 +27,18 @@ LEFT JOIN ( SELECT request_section_id as сt_rsid, value as city
 LEFT JOIN ( SELECT request_section_id as n_rsid, value as nick
             FROM [values] WHERE title LIKE 'Ник%')
     ON n_rsid = request_section_id
+LEFT JOIN ( SELECT request_section_id as pr_rsid, value as pron
+            FROM [values] WHERE title LIKE 'Транскрипция ника (для ведущих)')
+    ON pr_rsid = request_section_id
+LEFT JOIN ( SELECT request_section_id as ch_rsid, value as chars
+            FROM [values] WHERE title = 'Имя персонажа' OR title = 'Откуда персонаж')
+    ON ch_rsid = request_section_id
+LEFT JOIN ( SELECT request_id as ch_rid, value as single_char
+            FROM [values] WHERE title = 'Имя персонажа')
+    ON ch_rid = request_id
+LEFT JOIN ( SELECT request_id as fn_rid, value as fandom
+            FROM [values] WHERE title LIKE 'Фэндом%')
+    ON fn_rid = request_id
 LEFT JOIN ( SELECT request_section_id as r_rsid, value as phone
             FROM [values] WHERE title LIKE 'Мобильный телефон%')
     ON r_rsid = request_section_id
