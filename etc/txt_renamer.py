ce79fdd9b89bd0b10216e0e6c32f3afe87724464
Himura Kazuto <glagol15@gmail.com>
2021-08-03 20:47:36 +0300

    new usage example

--- etc/txt_renamer.py
+++ etc/txt_renamer.py
@@ -2,38 +2,12 @@
 import os
 import re
 
-d = "C:\\Users\\himura\\Desktop\\чб конкурс"
-pattern = '385%s. конкурс чб - %s'
-r = re.compile('(.+)(\.\w+)')
+d = "/media/data/Clouds/YandexDisk/Fests/ANI-SHINAI 2021/Fest/партнеры"
 
-data = """
-амнезия орион
-затана дс комиксф
-веном человек паук
-кот сильвестр загадочные истории
-микки маус
-безликий унесенные призраками
-шелдон купер теория большого взрыва эффект доллера
-круэлла де виль
-крик очень страшное кино
-по кунг фу панда
-мисаки президент студсовета горничная
-кид соул итер
-денни призрак денни
-мартин мадагаскар
-бетти буп
-битлджюс 
-слендермен
-джек кошмар перед рождеством
-монокума Данганронпа - Школа надежды и безнадёжные школьники
-""".split('\n')
-
+i=605
 for _, f in enumerate(os.listdir(d)):
-    name, ext = r.match(f).groups()
-    i = int(name)
-    ch = chr(ord('a') + i - 1)
-    title = f'{name}. {data[i]}'
-    new_name = pattern % (ch, title) + ext
+    i += 5
+    new_name = str(i) + " I. " + f.replace('_', ' - ').replace(' - .','.')
     print(f'{f}->{new_name}')
     old_path = os.path.join(d, f)
     new_path = os.path.join(d, new_name)
