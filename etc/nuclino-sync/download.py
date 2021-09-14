8371f6b547e0938cddbf3772100db6e6dc877485
Vladislav Glagolev (DevExpress) <vladislav.glagolev@devexpress.com>
2021-09-14 12:57:17 +0300

    Fail on error

--- etc/nuclino-sync/download.py
+++ etc/nuclino-sync/download.py
@@ -34,4 +34,6 @@ if __name__ == "__main__":
         print(f'error: the {cookie_var} environment variable is required.', file=sys.stderr)
         exit(1)
     
-    get_backup(b64decode(os.environ[cookie_var]).decode(), args.workspace_id, args.out_path)
+    success = get_backup(b64decode(os.environ[cookie_var]).decode(), args.workspace_id, args.out_path)
+
+    exit(0 if success else 2)
