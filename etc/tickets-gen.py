83c42bb627be3c38a69d5ab6b8d11063725b82c2
Himura Kazuto <glagol15@gmail.com>
2021-09-27 23:39:51 +0300

    QRs in tickets

--- etc/tickets-gen.py
+++ etc/tickets-gen.py
@@ -1,37 +1,36 @@
 import secrets
 from datetime import datetime
+from tempfile import mkdtemp
 import csv
 import os
+import qrcode
 
 types = {
     '01': {
         'descr': 'Билет «Тануки»',
-        'image': '1. Тануки.jpg',
-        'start': 300,
-        'total': 110
-    },
-    '02': {
-        'descr': 'Билет «Кицунэ»',
-        'image': '2. Кицунэ.jpg',
         'start': 300,
         'total': 60
     },
+    '02': {
+        'descr': 'Билет «Кицунэ»',
+        'start': 300,
+        'total': 25
+    },
     '03': {
         'descr': 'VIP Билет «Ооками»',
-        'image': '3. Оками.jpg',
         'start': 300,
-        'total': 30
+        'total': 20
     }
 }
 
-image_base_path = r'D:\Clouds\YandexDisk\Fests\Yuki no Odori\Кроли\Билетные кроли'
-target_csv_path = r'D:\Clouds\YandexDisk\Fests\Yuki no Odori 10\Tickets\%s.csv'
+target_csv_path = r'D:\Clouds\YandexDisk\Fests\Yuki no Odori 11\Tickets\%s.csv'
 pool = ['A', 'B', 'D', 'E', 'F', 'H', 'K', 'L', 'M', 'P', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'] + \
        [chr(ord('1') + i) for i in range(9)]
 
 if not os.path.exists(os.path.split(target_csv_path)[0]):
     os.makedirs(os.path.split(target_csv_path)[0])
 
+qr_dir = mkdtemp()
 
 def make_codes(how_many):
     return [''.join([secrets.choice(pool) for _ in range(9)]) for _ in range(how_many)]
@@ -40,27 +39,27 @@ def make_codes(how_many):
 target_csv_path = target_csv_path % datetime.now().strftime('%y%m%d%H%M%S')
 report = {}
 
-with open(target_csv_path, 'w', encoding='utf-16', newline='') as f:
+with open(target_csv_path, 'w', encoding='utf-8', newline='') as f:
     w = csv.writer(f, delimiter=',', quotechar='"')
     w.writerow([
-    #    'ds',
         'code'
         , 'ser'
         , 'num'
         , 'descr'
-    #    , 'img'
+        , 'qr'
     ])
     for ser, data in types.items():
         report[ser] = {'from': data['start']}
         for i, code in enumerate(make_codes(data['total'])):
             num = data['start'] + i
+            qr_img_path = os.path.join(qr_dir, code + '.png')
+            qrcode.make(code).save(qr_img_path)
             row = [
-            #    f"{ser}-{num}",
                 code
                 , ser
                 , num
                 , data['descr']
-            #    , os.path.join(image_base_path, data['image'])
+                , qr_img_path
             ]
             w.writerow(row)
             report[ser]['to'] = num
