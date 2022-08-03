b66fe5bcf714a1db5f230985f671f5d75891c433
Himura Kazuto <glagol15@gmail.com>
2022-08-03 21:01:44 +0400

    improve checker

--- etc/checker.py
+++ etc/checker.py
@@ -3,19 +3,37 @@
 import os
 import re
 import csv
+import unicodedata
 
-csv_path = r"C:\Users\glago\Desktop\AtomCosCon 22 - Заявки.csv"
-id_row = '№'
+csv_path = r"C:\Users\glago\YandexDisk\Fests\AtomCosCon 2022\AtomCosCon 22 - Заявки.csv"
+id_row = '#'
 
-folder_path = r"C:\Temp\AtomCosCon 2022\Files"
+folder_path = r"C:\Users\glago\YandexDisk\Fests\AtomCosCon 2022\Tracks"
 id_regex_filename = r"^(?P<id>\d{3})"
 
+
+def make_name(d):
+    return to_filename(f"{d['#']}. {d['Начало']}. {d['Категория']}. {d['Название номера']}")
+
+
+def to_filename(string):
+    filename = string.replace('й', "<икраткое>")
+    filename = unicodedata.normalize('NFD', filename).encode('cp1251', 'replace').decode('cp1251')
+    filename = filename.replace("<икраткое>", 'й')
+    filename = filename.replace(':', "")\
+                       .replace('|', "-").replace('/', "-").replace('\\', "-")\
+                       .replace('"', "'")\
+                       .replace('’', "'")\
+                       .replace(' ,', ", ")\
+                       .replace('  ', " ")
+    filename = ''.join(i if i not in "*?<>" else '' for i in filename)
+    return filename
 
 with open(csv_path, 'r', encoding='utf-8') as f:
     reader = csv.reader(f)
     head = reader.__next__()
     csv_data = {int(row[head.index(id_row)]):
-                {head[i]: row[i].strip() for i in range(len(head)) if i != head.index(id_row)} for row in reader}
+                {head[i]: row[i].strip() for i in range(len(head))} for row in reader if row[head.index(id_row)]}
 
 dir_data = dict()
 for file_name in os.listdir(folder_path):
@@ -23,4 +41,4 @@ for file_name in os.listdir(folder_path):
 
 for num, d in csv_data.items():
     if num not in dir_data.keys():
-        print(f"[NO FILE] {num}. {d['Название номера']}")
+        print(f"[NO FILE for № {d['№']} ] {make_name(d)}")
